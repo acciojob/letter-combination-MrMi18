c7b1afc01d5240d3c0c6e2173673374253774f3a
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-06-01 10:29:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  if (!digits) return [];
+
+    const phoneMap = {
+        "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+        "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+    };
+
+    let result = [];
+
+    function backtrack(index, currentStr) {
+        if (index === digits.length) {
+            result.push(currentStr);
+            return;
+        }
+
+        let letters = phoneMap[digits[index]];
+        for (let letter of letters) {
+            backtrack(index + 1, currentStr + letter);
+        }
+    }
+
+    backtrack(0, "");
+    return result;
 }
 
 module.exports = letterCombinations;
